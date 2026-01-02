c14c0c3c165d164f70201f41ac6a0fa5b1afac70
Michal Przygoda <michalprzygoda17@gmail.com>
2026-01-03 00:41:21 +0100

    init MVC

---
+++ FilmDB/Models/FilmModels.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmDB.Models
+{
+    public class FilmModels
+    {
+        public int  ID { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+    }
+}
